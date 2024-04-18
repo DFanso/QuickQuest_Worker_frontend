644da9b6516964e85ec6225f5ea3774a59e0c818
Leo Gavin <leogavin123@outlook.com>
2024-04-18 20:13:32 +0530

    feat: paypal account connect

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,12 @@ type AuthenticatedLayoutProps = {
 export default function RootLayout({ children }: { children: React.ReactNode; }) {
   return (
     <html lang="en">
+      <head>
+        <title>Quick-Quest</title>
+        <meta name="description" content="Book your favorite quest with Quick Quest!" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={inter.className}>
         <AuthProvider>
           <AuthenticatedLayout>
